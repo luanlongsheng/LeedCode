6b4d3991ac7f091ab2c7cccb1603d9fa48a1aef2
luanlongsheng <2683961566@qq.com>
2020-02-29 18:25:16 +0800

    二叉树的最小深度

---
+++ Leetcode111.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: Leetcode111.cpp
+	> Author:lls 
+	> Mail:luanlongshengv@163.com 
+	> Created Time: 2020年02月29日 星期六 18时24分39秒
+ ************************************************************************/
+
+int minDepth(struct TreeNode* root){
+    if (root == NULL) return 0;
+    if (root->left == NULL && root->right == NULL) return 1;
+    if (root->left == NULL || root->right == NULL) {
+        return minDepth(root->left ? root->left : root->right) + 1;
+    }
+    return fmin(minDepth(root->left), minDepth(root->right)) + 1;
+}
